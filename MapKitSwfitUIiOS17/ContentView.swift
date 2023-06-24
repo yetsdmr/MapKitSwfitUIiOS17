2aa3fa9bc21d2fceb30cecb414ccff137cf76ad7
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-06-24 09:59:55 +0300

    Initial Commit

---
+++ MapKitSwfitUIiOS17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapKitSwfitUIiOS17
+//
+//  Created by Yunus Emre Taşdemir on 24.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapKitSwfitUIiOS17/MapKitSwfitUIiOS17App.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitSwfitUIiOS17App.swift
+//  MapKitSwfitUIiOS17
+//
+//  Created by Yunus Emre Taşdemir on 24.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct MapKitSwfitUIiOS17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
